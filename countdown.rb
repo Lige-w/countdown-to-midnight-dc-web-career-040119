f702fd6de7281206251481f169bd516a54bfc00f
Lige <lige.williamson@gmail.com>
2019-02-08 23:46:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  count = n
+  while count > 1
+    puts "#{count} SECOND(S)"
+    count -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
